f4277bac8428a22cfdee8308ac44671b1ec11980
Peter Buckner <peter.buckner@gmail.com>
2020-06-06 20:06:57 -0700

    update version

--- XPython/plugin.c
+++ XPython/plugin.c
@@ -33,7 +33,7 @@ const char *pythonPluginsPath = "./Resources/plugins/PythonPlugins";
 const char *pythonInternalPluginsPath = "./Resources/plugins/XPPython3";
 
 static const char *pythonPluginName = "XPPython3";
-const char *pythonPluginVersion = "3.0.0.a2";
+const char *pythonPluginVersion = "3.0.0.a3";
 const char *pythonPluginSig  = "avnwx.xppython3";
 static const char *pythonPluginDesc = "X-Plane interface for Python 3";
 static const char *pythonStopCommand = "XPPython3/stopScripts";

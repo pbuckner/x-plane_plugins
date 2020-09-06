4105943e4046814616ecef638d9899d519827c46
Peter Buckner <peter.buckner@gmail.com>
2020-09-06 09:44:54 -0700

    b2

--- XPython/plugin.c
+++ XPython/plugin.c
@@ -34,7 +34,7 @@ const char *pythonPluginsPath = "./Resources/plugins/PythonPlugins";
 const char *pythonInternalPluginsPath = "./Resources/plugins/XPPython3";
 
 static const char *pythonPluginName = "XPPython3";
-const char *pythonPluginVersion = "3.0.0.b1 - for Python " PYTHONVERSION;
+const char *pythonPluginVersion = "3.0.0.b2 - for Python " PYTHONVERSION;
 const char *pythonPluginSig  = "avnwx.xppython3";
 static const char *pythonPluginDesc = "X-Plane interface for Python 3";
 static const char *pythonDisableCommand = "XPPython3/disableScripts";

c8b66f88bab1e4261600517b12ddcc3e91a74917
Peter Buckner <peter.buckner@gmail.com>
2020-10-14 21:18:06 -0700

    add log and sys_log functions

--- XPython/stubs/XPPython.py
+++ XPython/stubs/XPPython.py
@@ -9,3 +9,18 @@ def XPPythonGetDicts():
 
 def XPPythonGetCapsules():
     return {}
+
+
+def XPSystemLog(s: str) -> None:
+    """
+    Print string to X-Plane Log.txt (automatically append a single \n)
+    Will fflush() after each write.
+    """
+
+
+def XPPythonLog(s: str) -> None:
+    """
+    Print string to X-Plane XPPython3.log (automatically append a single \n)
+    Will _not_ fflush after each write. To fflush(), simply call with zero-length
+    string: XPPythonLog('')
+    """

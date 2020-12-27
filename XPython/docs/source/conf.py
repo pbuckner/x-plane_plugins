d4590a18b33dd8284a7b01e43aaa31e644733503
Peter Buckner <peter.buckner@gmail.com>
2020-12-27 15:09:27 -0800

    update docs to 3.0.8

--- XPython/docs/source/conf.py
+++ XPython/docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Peter Buckner'
 master_doc = 'index'
 
 # The full version, including alpha/beta/rc tags
-release = '3.0.7'
+release = '3.0.8'
 
 
 # -- General configuration ---------------------------------------------------

91bbbb24a537a25aecaca5709644870a153faa1a
Peter Buckner <peter.buckner@gmail.com>
2021-11-23 18:14:06 -0800

    versions.js bug

--- XPython/docs/source/_static/js/versions.js
+++ XPython/docs/source/_static/js/versions.js
@@ -2,7 +2,7 @@ $(function() {
     $('.version-selector').on('change', function() {
         console.log("selected", $(this).val());
         var files = location.pathname.split('/');
-        location.pathname = ([files[0], files[1], $(this).val()].concat(files.slice(3)).join('/');
+        location.pathname = [files[0], files[1], $(this).val()].concat(files.slice(3)).join('/');
     });
 });
     

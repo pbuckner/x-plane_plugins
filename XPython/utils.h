b73e33c2ff4b86f194c946bf6ed254e64832d851
Peter Buckner <peter.buckner@gmail.com>
2022-01-26 10:36:39 -0800

    support refactoring

--- XPython/utils.h
+++ XPython/utils.h
@@ -1,8 +1,6 @@
 #ifndef UTILS__H
 #define UTILS__H
 
-#include <Python.h>
-#include <stdbool.h>
 #define My_DOCSTR(name,method,parms,str) static const char name[] = method"($module, /, "parms")\n--\n\n"str
 extern const char *objRefName;
 extern const char *commandRefName;
@@ -13,10 +11,13 @@ extern PyObject *windowIDCapsules;
 extern PyObject *xppythonDicts;
 extern PyObject *xppythonCapsules;
 extern FILE *pythonLogFile;
+extern int pythonDebugs;
+extern int pythonWarnings;
 
 void dbg(const char *msg);
 float getFloatFromTuple(PyObject *seq, Py_ssize_t i);
 long getLongFromTuple(PyObject *seq, Py_ssize_t i);
+void pythonLogException(void);
 
 PyObject *getPtrRef(void *ptr, PyObject *dict, const char *refName);
 PyObject *getPtrRefOneshot(void *ptr, const char *refName);
@@ -25,6 +26,11 @@ void removePtrRef(void *ptr, PyObject *dict);
 char *get_moduleName();
 PyObject *get_pluginSelf();
 char *objToStr(PyObject *item);
+char *objDebug(PyObject *item);
 void pythonLogWarning(const char *msg);
 void pythonDebug(const char *fmt, ...);
+
+extern const char *pythonPluginsPath;
+extern const char *pythonInternalPluginsPath;
+
 #endif

7a4c4e12223083fd8ddf495d289b5460dda0c238
Peter Buckner <peter.buckner@gmail.com>
2022-11-25 15:02:24 -0800

    update release to 'beta' so docs get set properly

--- XPython/docs/source/conf.py
+++ XPython/docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Peter Buckner'
 master_doc = 'index'
 
 # The full version, including alpha/beta/rc tags
-release = '3.1.5'
+release = 'beta'
 
 # -- general configuration ---------------------------------------------------
 

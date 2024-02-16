b76d0f1abc2628f49008e99e8a160aad794ff1ac
Peter Buckner <peter.buckner@gmail.com>
2024-02-16 10:43:56 -0800

    note this doc release is beta

--- XPython/docs/source/conf.py
+++ XPython/docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Peter Buckner'
 master_doc = 'index'
 
 # The full version, including alpha/beta/rc tags
-release = 'latest'
+release = 'beta'
 
 # -- general configuration ---------------------------------------------------
 

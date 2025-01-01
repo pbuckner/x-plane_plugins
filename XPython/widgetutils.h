4de8a62cd2199fc803d9c79b53c783ff29278018
Peter Buckner <pbuck@mini.local>
2024-12-31 18:34:17 -0800

    typing

--- XPython/widgetutils.h
+++ XPython/widgetutils.h
@@ -1,2 +1,6 @@
-void convertMessagePythonToC();
-
+#ifndef WIDGETUTILS__H
+#define WIDGETUTILS__H 1
+#include <Python.h>
+#include <Widgets/XPWidgetUtils.h>
+void convertMessagePythonToC(XPWidgetMessage, PyObject *, PyObject *, PyObject *, XPWidgetID *, intptr_t *, intptr_t *);
+#endif

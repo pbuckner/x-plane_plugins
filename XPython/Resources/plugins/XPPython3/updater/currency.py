d144741e613d209a0c4eca2f6b4f6e8c9e0328f5
Peter Buckner <pbuck@xppython3.org>
2025-01-20 13:38:51 -0800

    type hinting

--- XPython/Resources/plugins/XPPython3/updater/currency.py
+++ XPython/Resources/plugins/XPPython3/updater/currency.py
@@ -1,13 +1,14 @@
+from typing import Self
 import XPPython3.updater.version as version
 
 
 class Currency:
-    def __init__(self, self_version, beta_version, new_version):
+    def __init__(self: Self, self_version: str, beta_version: str, new_version: str) -> None:
         self.self_version = self_version  # self.Version
         self.beta_version = beta_version  # self.beta_version
         self.new_version = new_version    # self.new_version
 
-    def get_currency(self, try_beta):
+    def get_currency(self: Self, try_beta: bool) -> str:
         uptodate, change_to = version.calc_update(try_beta, self.self_version, self.new_version, self.beta_version)
         if uptodate:
             if self.self_version == self.new_version:

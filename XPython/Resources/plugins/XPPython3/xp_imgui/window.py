21f3a3cc3982702efff7f1b5400b4b66403ae182
Peter Buckner <pbuck@xppython3.org>
2025-01-20 13:44:11 -0800

    type hinting and pylint

--- XPython/Resources/plugins/XPPython3/xp_imgui/window.py
+++ XPython/Resources/plugins/XPPython3/xp_imgui/window.py
@@ -37,6 +37,7 @@
 # YOU SHOULD NOT NEED TO MAKE ANY CHANGES TO THIS FILE -- if you do, it's most
 # likely you're fixing a bug I should know about, so tell me about it -- support@xppython3.org
 
+from typing import Any, Optional, Self, Callable
 import traceback
 from collections import namedtuple
 import sys
@@ -46,9 +47,12 @@ except ImportError:
     print("[XPPython3] OpenGL not found. Use XPPython3 Pip Package Installer to install 'pyopengl' package and restart.")
     raise
 
-import imgui  # type: ignore
+
+import imgui
 from XPPython3 import xp
+from XPPython3.xp_typing import XPLMWindowID, XPLMCursorStatus
 from .xprenderer import XPRenderer
+
 
 CreateWindow_t = namedtuple('CreateWindow_t', ['left', 'top', 'right', 'bottom',
                                                'visible',
@@ -63,14 +67,14 @@ CreateWindow_t = namedtuple('CreateWindow_t', ['left', 'top', 'right', 'bottom',
                                                'handleRightClickFunc'])
 
 
-def loge(s):
+def loge(s: str) -> None:
     """ log with exception traceback """
-    xp.log('{}\n{}'.format(s, traceback.format_exc()))
+    xp.log(f'{s}\n{traceback.format_exc()}')
 
 
 class Window:
-    def __init__(self, left=100, top=200, right=200, bottom=100, visible=0, 
-                 draw=None, refCon=None, decoration=1, layer=1):
+    def __init__(self: Self, left: int = 100, top: int = 200, right: int = 200, bottom: int = 100, visible: int = 0,
+                 draw: Optional[Callable] = None, refCon: Optional[Any] = None, decoration: int = 1, layer: int = 1) -> None:
         self.createWindow_t = CreateWindow_t(left, top, right, bottom, visible, draw, None, None, None, None,
                                              refCon, decoration, layer, None)
         self.stop = False
@@ -80,10 +84,15 @@ class Window:
         self.modelviewMatrixRef = xp.findDataRef("sim/graphics/view/modelview_matrix")
         self.viewportRef = xp.findDataRef("sim/graphics/view/viewport")
         self.projectionMatrixRef = xp.findDataRef("sim/graphics/view/projection_matrix")
-        self.modelview = []
-        self.projection = []
-        self.viewport = []
+        self.modelview: list[float] = []
+        self.projection: list[float] = []
+        self.viewport: list[int] = []
 
+        # Set in imguiDrawWindow by call from xp.getWindowGeometry()
+        self.mLeft = 0
+        self.mRight = 0
+        self.mTop = 0
+        self.mBottom = 0
 
         # Initialize
         self.imgui_context = imgui.create_context()
@@ -97,7 +106,6 @@ class Window:
             self.io.config_resize_windows_from_edges = False
         except AttributeError:
             self.io.config_windows_resize_from_edges = False
-            
 
         # self.io.ini_file_name = None
         self.io.config_flags = imgui.CONFIG_NAV_NO_CAPTURE_KEYBOARD
@@ -125,7 +133,7 @@ class Window:
         self.io.key_map[imgui.KEY_Y] = xp.VK_Y
         self.io.key_map[imgui.KEY_Z] = xp.VK_Z
 
-        self.renderer = XPRenderer()
+        self.renderer = XPRenderer(self)
 
         # Here, we create the XP window, but pass to it "our" callbacks (e.g., self.drawWindow, self.handleMouseClick)
         # "Our" callbacks will do whatever they need to do and then call the user-provided callbacks
@@ -138,7 +146,7 @@ class Window:
             self.imguiHandleRightClick)
         xp.bringWindowToFront(self.windowID)
 
-    def delete(self):
+    def delete(self: Self) -> None:
         self.stop = True
         try:
             if self.imgui_context is not None:
@@ -148,19 +156,20 @@ class Window:
                 imgui.destroy_context(self.imgui_context)
                 self.imgui_context = None
             if self.fontTextureId is not None:
-                GL.glDeleteTextures([self.fontTextureId])
+                GL.glDeleteTextures(1, [self.fontTextureId])
                 self.fontTextureId = None
             if self.windowID is not None:
                 xp.destroyWindow(self.windowID)
                 self.windowID = None
-        except Exception:
+        except Exception:  # pylint: disable=broad-except
             loge("Exception while trying to delete window")
 
-    def imguiHandleMouseClick(self, inWindowID, x, y, inMouse, inRefCon):
+    def imguiHandleMouseClick(self: Self, _inWindowID: int, x: int, y: int, inMouse: int, _inRefCon: Any) -> int:
         # if not xp.hasKeyboardFocus(inWindowID):
         #     xp.takeKeyboardFocus(inWindowID)
         # First, handle imgui mouse click
-        imgui.set_current_context(self.imgui_context)
+        if self.imgui_context:
+            imgui.set_current_context(self.imgui_context)
         imguiX, imguiY = self.translateToImguiSpace(x, y)
         self.io.mouse_pos = imguiX, imguiY
         if inMouse in (xp.MouseDown, xp.MouseDrag):
@@ -171,18 +180,22 @@ class Window:
         # Finally, return 1 to indicate we've handled the mouse click
         return 1
 
-    def setTitle(self, title):
-        xp.setWindowTitle(self.windowID, title)
+    def setTitle(self: Self, title: str) -> None:
+        if self.windowID is not None:
+            xp.setWindowTitle(self.windowID, title)
 
-    def imguiDrawWindow(self, windowID, inRefCon):
+    def imguiDrawWindow(self: Self, windowID: XPLMWindowID, inRefCon: Any):
         self.updateMatrices()
-        imgui.set_current_context(self.imgui_context)
+        if self.imgui_context is not None:
+            imgui.set_current_context(self.imgui_context)
         if self.stop:
             return
         try:
             try:
                 # Set site of the window into which we'll be writing imgui
-                (self.mLeft, self.mTop, self.mRight, self.mBottom) = xp.getWindowGeometry(windowID)
+                geom = xp.getWindowGeometry(windowID)
+                if geom is not None:
+                    (self.mLeft, self.mTop, self.mRight, self.mBottom) = geom
                 io = imgui.get_io()
 
                 hasKeyboardFocus = self.hasInputFocus()
@@ -191,8 +204,8 @@ class Window:
                 elif not io.want_text_input and hasKeyboardFocus:
                     self.requestInputFocus(False)
                     # reset keysdown otherwise we'll think any keys used to defocus the keyboard are still down!
-                    for x in range(len(io.keys_down)):
-                        io.keys_down[x] = False
+                    for i, _val in enumerate(io.keys_down):
+                        io.keys_down[i] = False
 
                 io.display_size = self.mRight - self.mLeft, self.mTop - self.mBottom
                 io.display_fb_scale = 1.0, 1.0
@@ -200,8 +213,8 @@ class Window:
                     imgui.new_frame()
                     imgui.set_next_window_position(0.0, 0.0, imgui.ALWAYS)
                     imgui.set_next_window_size(self.mRight - self.mLeft, self.mTop - self.mBottom, imgui.ALWAYS)
-                except Exception as e:
-                    xp.log("Failure with window pos / size: {}".format(e))
+                except Exception as e:  # pylint: disable=broad-except
+                    xp.log(f"Failure with window pos / size: {e}")
                     raise
 
                 imgui.begin("X-Plane",
@@ -211,9 +224,10 @@ class Window:
                 imgui.end()
                 imgui.render()
 
-                imgui.set_current_context(self.imgui_context)
-                self.renderer.render(self, imgui.get_draw_data())
-            except Exception:
+                if self.imgui_context is not None:
+                    imgui.set_current_context(self.imgui_context)
+                self.renderer.render(imgui.get_draw_data())
+            except Exception:  # pylint: disable=broad-except
                 loge('oops')
                 self.stop = True
                 return
@@ -221,28 +235,30 @@ class Window:
             loge("Exception in drawwindow")
             raise
 
+    def hasInputFocus(self: Self) -> int:
+        if self.windowID is None:
+            return False
+        return xp.hasKeyboardFocus(self.windowID)
 
-    def hasInputFocus(self):
-        ret = xp.hasKeyboardFocus(self.windowID)
-        return ret
+    def requestInputFocus(self: Self, req: bool) -> None:
+        if self.windowID is not None:
+            xp.takeKeyboardFocus(self.windowID if req else 0)
 
-    def requestInputFocus(self, req):
-        xp.takeKeyboardFocus(self.windowID if req else None)
-
-    def imguiHandleRightClick(self, inWindowID, x, y, inMouse, inRefCon):
+    def imguiHandleRightClick(self: Self, _inWindowID: XPLMWindowID, _x, _y, _inMouse, _inRefCon) -> int:
         return 1
 
-    def imguiHandleCursor(self, inWindowID, x, y, inRefCon):
+    def imguiHandleCursor(self: Self, _inWindowID: XPLMWindowID, _x: int, _y: int, _inRefCon: Any) -> XPLMCursorStatus:
         return xp.CursorDefault
 
-    def imguiHandleMouseWheel(self, inWindowID, x, y, wheel, clicks, inRefCon):
+    def imguiHandleMouseWheel(self: Self, _inWindowID: XPLMWindowID, _x, _y, _wheel, _clicks, _inRefCon: Any) -> int:
         return 1
 
-    def imguiHandleKey(self, inWindowID, inKey, inFlags, inVirtualKey, inRefCon, losingFocus):
+    def imguiHandleKey(self: Self, _inWindowID: XPLMWindowID, inKey: int, inFlags: int, inVirtualKey: int, _inRefCon: Any, losingFocus: int) -> None:
         if losingFocus:
             self.requestInputFocus(False)
             return
-        imgui.set_current_context(self.imgui_context)
+        if self.imgui_context is not None:
+            imgui.set_current_context(self.imgui_context)
         io = imgui.get_io()
         if io.want_text_input:
             # if you press and hold a key, the flags wiil actually be down, 0, 0, ..., up
@@ -251,7 +267,7 @@ class Window:
                 # print("inKey: {}, virtualKey: {}, Description: {}".format(inKey, inVirtualKey, xp.getVirtualKeyDescription(inVirtualKey & 0xff)))
                 io.keys_down[inVirtualKey & 0xff] = (inFlags & xp.UpFlag) != xp.UpFlag
             except IndexError:
-                loge('IndexError for inKey: {}, inVirtualKey: {} / {}'.format(inKey, inVirtualKey, inVirtualKey & 0xff))
+                loge(f'IndexError for inKey: {inKey}, inVirtualKey: {inVirtualKey} / {inVirtualKey & 0xff}')
                 return
             io.key_shift = (inFlags & xp.ShiftFlag) == xp.ShiftFlag
             io.key_alt = (inFlags & xp.OptionAltFlag) == xp.OptionAltFlag
@@ -259,11 +275,11 @@ class Window:
             try:
                 if (inFlags & xp.UpFlag) != xp.UpFlag and not io.key_ctrl and not io.key_alt and chr(inKey).isprintable():
                     io.add_input_character(inKey)
-            except Exception:
-                loge('failed to add key: {}, vk: {}'.format(inKey, inVirtualKey))
+            except Exception:  # pylint: disable=broad-except
+                loge(f'failed to add key: {inKey}, vk: {inVirtualKey}')
         return
 
-    def translateToImguiSpace(self, x, y):
+    def translateToImguiSpace(self: Self, x: float, y: float) -> tuple[float, float]:
         imguiX = x - self.mLeft
         if imguiX < 0 or imguiX > (self.mRight - self.mLeft):
             return -sys.float_info.max, -sys.float_info.max
@@ -272,31 +288,31 @@ class Window:
             return -sys.float_info.max, -sys.float_info.max
         return imguiX, imguiY
 
-    def translateImguiToBoxel(self, x, y):
+    def translateImguiToBoxel(self: Self, x: float, y: float) -> tuple[int, int]:
         return int(self.mLeft + x), int(self.mTop - y)
 
-    def boxelsToNative(self, x, y):
+    def boxelsToNative(self: Self, x: float, y: float) -> tuple[int, int]:
         # vrEnabledRef = XPLMFindDataRef("sim/graphics/VR/enabled");
         try:
-            boxelPos = [float(x), float(y), 0, 1]
+            boxelPos = (float(x), float(y), 0, 1)
 
             eye = self.multMatrixVec4f(self.modelview, boxelPos)
             ndc = self.multMatrixVec4f(self.projection, eye)
             outX = int((ndc[0] * 0.5 + 0.5) * self.viewport[2] + self.viewport[0])
             outY = int((ndc[1] * 0.5 + 0.5) * self.viewport[3] + self.viewport[1])
-        except Exception as e:
-            xp.log("Exception in Boxesl to Native: {}".format(e))
+        except Exception as e:  # pylint: disable=broad-except
+            xp.log(f"Exception in Boxesl to Native: {e}")
             raise
         return outX, outY
 
     @staticmethod
-    def multMatrixVec4f(m, v):
-        return [v[0] * m[0] + v[1] * m[4] + v[2] * m[8] + v[3] * m[12],
+    def multMatrixVec4f(m: list[float], v: tuple[float, float, float, float]) -> tuple[float, float, float, float]:
+        return (v[0] * m[0] + v[1] * m[4] + v[2] * m[8] + v[3] * m[12],
                 v[0] * m[1] + v[1] * m[5] + v[2] * m[9] + v[3] * m[13],
                 v[0] * m[2] + v[1] * m[6] + v[2] * m[10] + v[3] * m[14],
-                v[0] * m[3] + v[1] * m[7] + v[2] * m[11] + v[3] * m[15]]
+                v[0] * m[3] + v[1] * m[7] + v[2] * m[11] + v[3] * m[15])
 
-    def updateMatrices(self):
+    def updateMatrices(self: Self):
         xp.getDatavf(self.modelviewMatrixRef, self.modelview, 0, 16)
         xp.getDatavf(self.projectionMatrixRef, self.projection, 0, 16)
         xp.getDatavi(self.viewportRef, self.viewport, 0, 4)

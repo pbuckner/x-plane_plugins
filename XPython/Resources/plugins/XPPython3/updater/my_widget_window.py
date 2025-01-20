d995828ab44dc36e1757f1b7f7b0221b6c2225d0
Peter Buckner <pbuck@xppython3.org>
2025-01-20 13:39:02 -0800

    type hinting

--- XPython/Resources/plugins/XPPython3/updater/my_widget_window.py
+++ XPython/Resources/plugins/XPPython3/updater/my_widget_window.py
@@ -1,7 +1,8 @@
+from typing import Self, Optional
 from XPPython3 import xp_typing
 
 
 class MyWidgetWindow:
-    def __init__(self):
-        self.widgetID: xp_typing.XPWidgetID = None
-        self.widgets = {}
+    def __init__(self: Self) -> None:
+        self.widgetID: Optional[xp_typing.XPWidgetID] = None
+        self.widgets: dict = {}

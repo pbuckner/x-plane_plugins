a43e0524bb5893b6bf5bb5aa92a5f77554e1fca2
Peter Buckner <pbuck@xppython3.org>
2025-10-30 10:34:06 -0700

    *** empty log message ***

--- XPython/encryption.cpp
+++ XPython/encryption.cpp
@@ -6,24 +6,24 @@ void setEncryptionLoader()
 {
   PyObject *cryptoModuleObj = PyImport_ImportModule("cryptography");
   if (! cryptoModuleObj) {
-    pythonLog("[XPPython3] Cryptography package not installed, XPPython3.xpye will not be supported. See Documentation.");
+    pythonLog("[XPPython3] Cryptography package not installed, XPPython3.xpyce will not be supported. See Documentation.");
     return;
   }
   Py_DECREF(cryptoModuleObj); // we needed to load it, just to verify it's available
 
-  PyObject *mod = PyImport_ImportModule("XPPython3.xpye");
+  PyObject *mod = PyImport_ImportModule("XPPython3.xpcye");
   if (mod) {
-    PyObject *function = PyObject_GetAttrString(mod, "XPYEPathFinder");
+    PyObject *function = PyObject_GetAttrString(mod, "XPYCEPathFinder");
     if (function) {
       PyObject *meta_path = PySys_GetObject("meta_path");
       PyList_Append(meta_path, function);
       PySys_SetObject("meta_path", meta_path);
-      pythonDebug("[XPPthon3] XPYEPathFinder initialized");
+      pythonDebug("[XPPthon3] XPYCEPathFinder initialized");
     } else {
-      pythonDebug("[XPPython3] Failed to initialize XPYEPathFinder.");
+      pythonDebug("[XPPython3] Failed to initialize XPYCEPathFinder.");
       Py_DECREF(mod);
     }
   } else {
-    pythonDebug("[XPPython3] Failed to load XPPython3.xpye module");
+    pythonDebug("[XPPython3] Failed to load XPPython3.xpyce module");
   }
 }
